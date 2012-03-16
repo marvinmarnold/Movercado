fd6f9ecf04dcfcab19dfc535ac964f55bc22d8f4
Marvin Arnold <marvinmarnold@gmail.com>
2012-03-16 10:52:25 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Movercado::Application.config.secret_token = '3434b1d91cdc27e3f201de5c90f4a9dc225e582bbf6fcc315b52815a0f834c343b04aacd6fb49ab1f4302bd3c6f5638f38c416de0c6d5f0a6d5e46e7e2ed6a06'
