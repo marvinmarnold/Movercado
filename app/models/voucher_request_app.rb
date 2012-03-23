a4bd12ab3e7c6631c35346da27d94cdab5457424
Marvin Arnold <marvinmarnold@gmail.com>
2012-03-23 11:24:31 +0200

    fix

--- app/models/voucher_request_app.rb
+++ app/models/voucher_request_app.rb
@@ -1,5 +1,5 @@
 class VoucherRequestApp < App
   def process(message_object)
-    respond_to_message(message_object, (I18n.t 	'voucher_request_app.code', :code => message_object.sender_phone.user.actor_codes.create.code), "")
+    respond_to_message(message_object, (I18n.t 	'voucher_request_app.code', :code => "111#{message_object.sender_phone.user.actor_codes.create(:app => self).code}".upcase), "")
 	end
 end
