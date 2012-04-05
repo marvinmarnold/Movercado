9de47bed1b5891da64b3874798caf0bd6083d48c
Marvin Arnold <marvinmarnold@gmail.com>
2012-04-05 12:01:07 +0200

    added message export to csv

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,12 +11,6 @@ class User < ActiveRecord::Base
   has_many :actor_codes
   has_many :sales_datum
   
-    devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-  
   accepts_nested_attributes_for :phones, :reject_if => lambda { |p| p[:number].blank? }, :allow_destroy => true
   accepts_nested_attributes_for :user_app_vars, :reject_if => lambda { |v| v[:role].blank? }
   
--- db/schema.rb
+++ db/schema.rb
@@ -200,29 +200,14 @@ ActiveRecord::Schema.define(:version => 20120327153124) do
   add_index "user_app_vars", ["user_id"], :name => "index_user_app_vars_on_user_id"
 
   create_table "users", :force => true do |t|
-    t.string   "email",                     :default => "", :null => false
-    t.string   "encrypted_password",        :default => "", :null => false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",             :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
     t.date     "birthday"
     t.string   "gender"
     t.integer  "num_actor_codes_generated", :default => 0
     t.integer  "num_children"
     t.string   "marital_status"
     t.string   "nickname"
-    t.string   "token"
-    t.string   "key"
-    t.datetime "created_at",                                :null => false
-    t.datetime "updated_at",                                :null => false
+    t.datetime "created_at",                               :null => false
+    t.datetime "updated_at",                               :null => false
   end
-
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
